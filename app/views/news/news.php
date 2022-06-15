0247bd7cf7fa32b2220521a8fbc88f921fb2ca29
Daniel Brendel <dbrendel1988@gmail.com>
2022-06-15 22:38:01 +0200

    Margin bottom

--- app/views/news/news.php
+++ app/views/news/news.php
@@ -3,7 +3,7 @@
 
 	<div class="column is-8 is-vertical-margin">
 		<div class="content-padding">
-			<h2 class="is-font-headline">News from the Steam Community</h2>
+			<h2 class="is-font-headline is-headline-margin-bottom">News from the Steam Community</h2>
 
             <div class="news-content" id="news-content">
                 <center><i class="fas fa-spinner fa-spin"></i></center>
