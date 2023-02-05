84505335e032e5f159833b6fa30c876b1ab65549
Daniel Brendel <dbrendel1988@gmail.com>
2023-02-05 01:03:04 +0100

    OpenGraph meta info

--- app/views/layout.php
+++ app/views/layout.php
@@ -6,6 +6,11 @@
 		<meta name="author" content="Daniel Brendel">
 		<meta name="description" content="Official homepage of Casual Desktop Game">
 		<meta name="tags" content="daniel brendel, cdg, casual desktop game, steam">
+
+		<meta property="og:title" content="Casual Desktop Game{{ ((isset($page_title)) ? ' - ' . $page_title : '') }}">
+		<meta property="og:description" content="Official homepage of Casual Desktop Game">
+		<meta property="og:url" content="{{ url($_SERVER['REQUEST_URI']) }}">
+		<meta property="og:image" content="{{ asset('img/steam_shop.png') }}">
 		
 		<title>
 			Casual Desktop Game
